1cf53eaab16f35ae2c55eab48b1e2971afe1a5c3
gray2cool <gmanroper@gmail.com>
2025-10-21 19:23:08 -0400

    Grayson

--- main.cpp
+++ main.cpp
@@ -8,8 +8,8 @@
 #include "colors.h"  // Include predefined color codes
 
 // Easy-to-change constants
-const std::string NAME  = "Aren";  // <-- Change this for your name
-const std::string COLOR = RED;     // <-- Change this to BLUE, GREEN, etc. from colors.h
+const std::string NAME  = "Grayson";  // <-- Change this for your name
+const std::string COLOR = MAGENTA;     // <-- Change this to BLUE, GREEN, etc. from colors.h
 
 int main() {
     std::cout << COLOR << "Hello " << NAME << "!" << RESET << std::endl;
